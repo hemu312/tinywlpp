dd658cec5b26979d8608f815c432faa6b0424bd1
Hemant Kumar Meena <shyena.freelancer@gmail.com>
2024-07-08 22:58:20 +0530

    include wlr headers and fix c compilation

---
+++ tinywlpp.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define class     _class
+#define namespace _namespace
+#define static
+#define delete delete_
+
+extern "C" {
+#include <wlr/backend.h>
+#include <wlr/render/allocator.h>
+#include <wlr/render/wlr_renderer.h>
+#include <wlr/types/wlr_cursor.h>
+#include <wlr/types/wlr_compositor.h>
+#include <wlr/types/wlr_data_device.h>
+#include <wlr/types/wlr_input_device.h>
+#include <wlr/types/wlr_keyboard.h>
+#include <wlr/types/wlr_output.h>
+#include <wlr/types/wlr_output_layout.h>
+#include <wlr/types/wlr_pointer.h>
+#include <wlr/types/wlr_scene.h>
+#include <wlr/types/wlr_seat.h>
+#include <wlr/types/wlr_subcompositor.h>
+#include <wlr/types/wlr_xcursor_manager.h>
+#include <wlr/types/wlr_xdg_shell.h>
+#include <wlr/util/log.h>
+}
+
+#undef delete
+#undef class
+#undef namespace
+#undef static
+#endif
