cbf100b4eff5d1f4dfea1eab548bff63eabd6e8a
Hemant Kumar Meena <shyena.freelancer@gmail.com>
2024-07-09 15:42:02 +0530

    add server.handle_keybinding method

--- tinywlpp.h
+++ tinywlpp.h
@@ -74,7 +74,31 @@ namespace tinywlpp {
         struct wl_listener new_output;
 
     public:
-        
+        bool handle_keybinding(xkb_keysym_t sym) {
+            /*
+            * Here we handle compositor keybindings. This is when the compositor is
+            * processing keys, rather than passing them on to the client for its own
+            * processing.
+            *
+            * This function assumes Alt is held down.
+            */
+            switch (sym) {
+                case XKB_KEY_Escape:
+                    wl_display_terminate(this->wl_display);
+                    break;
+                case XKB_KEY_F1:
+                    /* Cycle to the next toplevel */
+                    if (wl_list_length(&this->toplevels) < 2) {
+                        break;
+                    }
+                    tinywlpp::toplevel *next_toplevel = wl_container_of(this->toplevels.prev, next_toplevel, link);
+                    focus_toplevel(next_toplevel, next_toplevel->xdg_toplevel->base->surface);
+                    break;
+                default:
+                    return false;
+            }
+            return true;
+        }
     };
 
     class output {
